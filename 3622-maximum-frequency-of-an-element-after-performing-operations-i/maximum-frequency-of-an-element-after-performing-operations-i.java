97672b0d15c657fb60c96121d8d15a621dbd22cf
Vivek Tripathi <tripathivivek2018@gmail.com>
2025-10-21 12:38:05 +0530

    Time: 5 ms (100.00%) | Memory: 58 MB (73.68%) - LeetSync

---
+++ 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int maxFrequency(int[] nums, int k, int numOperations) {
+
+        int max = 0, min = Integer.MAX_VALUE;
+
+        for (int i : nums) {
+            max = Math.max(max, i);
+            min = Math.min(min, i);
+        }
+
+        int[] freq = new int[max + 1];
+        int[] prefix = new int[max + 1];
+        for (int i : nums) {
+            freq[i]++;
+        }
+        for (int i = min; i <= max; i++) {
+            prefix[i] = prefix[i - 1] + freq[i];
+        }
+        int ans = 0;
+        for (int i = min; i <= max; i++) {
+            int low = 0;
+            if (i - k - 1 > 0) {
+                low = prefix[i - k - 1];
+            }
+            int high = 0;
+            if (i + k <= max) {
+                high = prefix[i + k];
+            } else {
+                high = prefix[max];
+            }
+            int toChange = high - low - freq[i];
+            ans = Math.max(ans, freq[i] + (toChange >= numOperations ? numOperations : toChange));
+        }
+
+        return ans;
+    }
+}
