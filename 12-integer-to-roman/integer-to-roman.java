166fec4e4c9d0e4428ad6d29a4e821909f8692ff
Vivek Tripathi <tripathivivek2018@gmail.com>
2025-10-29 23:41:36 +0530

    Time: 3 ms (98.62%) | Memory: 44.9 MB (20.94%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.java
+++ 12-integer-to-roman/integer-to-roman.java
@@ -1,59 +1,15 @@
 class Solution {
     public String intToRoman(int num) {
-        String result = ""; 
-        while (num >= 1000) {
-            result += "M";
-            num -= 1000;
+        int[] values =    {1000, 900, 500, 400, 100, 90,  50,  40,  10, 9,  5, 4, 1};
+        String[] symbols = {"M", "CM","D", "CD","C","XC","L","XL","X","IX","V","IV","I"};
+
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {   
+                num -= values[i];
+                result.append(symbols[i]);  
+            }
         }
-        if (num >= 900) {
-            result += "CM";
-            num -= 900;
-        }
-        if (num >= 500) {
-            result += "D";
-            num -= 500;
-        }
-        if (num >= 400) {
-            result += "CD";
-            num -= 400;
-        }
-        while (num >= 100) {
-            result += "C";
-            num -= 100;
-        }
-        if (num >= 90) {
-            result += "XC";
-            num -= 90;
-        }
-        if (num >= 50) {
-            result += "L";
-            num -= 50;
-        }
-        if (num >= 40) {
-            result += "XL";
-            num -= 40;
-        }
-        while (num >= 10) {
-            result += "X";
-            num -= 10;
-        }
-        if (num >= 9) {
-            result += "IX";
-            num -= 9;
-        }
-        if (num >= 5) {
-            result += "V";
-            num -= 5;
-        }
-        if (num >= 4) {
-            result += "IV";
-            num -= 4;
-        }
-        while (num >= 1) {
-            result += "I";
-            num -= 1;
-        }
-        
-        return result; 
+         return result.toString();  
     }
 }
