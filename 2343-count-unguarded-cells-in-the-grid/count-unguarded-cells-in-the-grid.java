696fcec774ed2b22cccd81fc5d7bf09e0ee5ac22
Vivek Tripathi <tripathivivek2018@gmail.com>
2025-11-02 23:26:42 +0530

    Time: 20 ms (83.94%) | Memory: 145.1 MB (5.11%) - LeetSync

--- 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.java
+++ 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.java
@@ -1,45 +1,49 @@
 class Solution {
+
+    public void markGuarded(int row,int col,int[][] arr){
+        for(int i=row-1;i>=0;i--){
+            if(arr[i][col]==2 || arr[i][col]==3) break;
+            arr[i][col] = 1;
+        }
+        for(int i=row+1;i<arr.length;i++){
+            if(arr[i][col]==2 || arr[i][col]==3) break;
+            arr[i][col] = 1;
+        }
+        for(int j=col-1;j>=0;j--){
+            if(arr[row][j]==2 || arr[row][j]==3) break;
+            arr[row][j] = 1;
+        }
+        for(int j=col+1;j<arr[0].length;j++){
+            if(arr[row][j]==2 || arr[row][j]==3) break;
+            arr[row][j] = 1;
+        }
+
+    }
     public int countUnguarded(int m, int n, int[][] guards, int[][] walls) {
-        int[][] grid = new int[m][n];
-        for (int[] g : guards) grid[g[0]][g[1]] = 1;
-        for (int[] w : walls) grid[w[0]][w[1]] = 2;
-        for (int i = 0; i < m; i++) {
-            boolean seen = false;
-            for (int j = 0; j < n; j++) { 
-                if (grid[i][j] == 1) seen = true;
-                else if (grid[i][j] == 2) seen = false;
-                else if (seen) grid[i][j] = 3;
-            }
-
-            seen = false;
-            for (int j = n - 1; j >= 0; j--) { 
-                if (grid[i][j] == 1) seen = true;
-                else if (grid[i][j] == 2) seen = false;
-                else if (seen) grid[i][j] = 3;
-            }
+        int[][] arr =new int[m][n];
+        for(int[] guard : guards){
+            int i = guard[0];
+            int j = guard[1];
+            arr[i][j] = 2;
         }
-        for (int j = 0; j < n; j++) {
-            boolean seen = false;
-            for (int i = 0; i < m; i++) { 
-                if (grid[i][j] == 1) seen = true;
-                else if (grid[i][j] == 2) seen = false;
-                else if (seen) grid[i][j] = 3;
-            }
-
-            seen = false;
-            for (int i = m - 1; i >= 0; i--) { 
-                if (grid[i][j] == 1) seen = true;
-                else if (grid[i][j] == 2) seen = false;
-                else if (seen) grid[i][j] = 3;
+        for(int[] wall : walls){
+            int i = wall[0];
+            int j = wall[1];
+            arr[i][j] = 3;       
+        }
+        for(int[] guard : guards){
+            int i = guard[0];
+            int j = guard[1];
+            markGuarded(i,j,arr);
+        }
+        
+        int c = 0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(arr[i][j]==0) c++;
             }
         }
 
-        int count = 0;
-        for (int i = 0; i < m; i++)
-            for (int j = 0; j < n; j++)
-                if (grid[i][j] == 0)
-                    count++;
-
-        return count;
+        return c;
     }
 }
